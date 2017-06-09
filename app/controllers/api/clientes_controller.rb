f669efdd66892f73811ee3a8e7173020244e0434
Gabriel Figueira <figueira.gabriel11@gmail.com>
2017-06-08 21:55:01 -0300

    Correção api

--- app/controllers/api/clientes_controller.rb
+++ app/controllers/api/clientes_controller.rb
@@ -1,14 +1,27 @@
 class Api::ClientesController < ApiController
 
   def salvar
-    cliente = Cliente.find_by(cpf: parametros_cliente[:cpf])
+    cliente = Cliente.find_by(cpf: params[:cpf])
+
+    parametros = {
+      nome: params[:nome],
+      cpf: params[:cpf],
+      logradouro: params[:logradouro],
+      numero: params[:numero],
+      bairro: params[:bairro],
+      cep: params[:cep],
+      cidade: params[:cidade],
+      telefone: params[:telefone],
+      email: params[:email],
+      data_sincronizacao: params[:data_sincronizacao]
+    }
 
     if cliente.present?
-      cliente.attributes = parametros_cliente
+      cliente.attributes = parametros
     else
-      cliente = Cliente.new(parametros_cliente)
+      cliente = Cliente.new(parametros)
     end
-    cliente.attributes = parametros_cliente
+    cliente.attributes = parametros
 
     if ! cliente.save
       render json: {response: "erro"}
@@ -18,28 +31,11 @@ class Api::ClientesController < ApiController
   end
 
   def excluir
-    cliente = Cliente.find_by(cpf: parametros_cliente[:cpf])
+    cliente = Cliente.find_by(cpf: parametros[:cpf])
     if cliente.destroy
       render json: {response: "ok"}
     else
       render json: {response: "erro"}
     end
   end
-
-  private
-    def parametros_cliente
-      params.require(:cliente).permit(
-        :nome,
-        :cpf,
-        :logradouro,
-        :numero,
-        :bairro,
-        :cep,
-        :cidade,
-        :telefone,
-        :email,
-        :data_cadastro,
-        :data_sincronizacao
-      )
-    end
 end
