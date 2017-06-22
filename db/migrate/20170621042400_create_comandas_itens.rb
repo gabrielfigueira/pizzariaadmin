2f1f2ff88a209f8c81942e01e68d984f666b77f4
Gabriel Figueira <figueira.gabriel11@gmail.com>
2017-06-22 20:03:06 -0300

    correcao migrate

--- db/migrate/20170621042400_create_comandas_itens.rb
+++ db/migrate/20170621042400_create_comandas_itens.rb
@@ -1,7 +1,7 @@
 class CreateComandasItens < ActiveRecord::Migration[5.0]
   def change
     create_table :comandas_itens do |t|
-      t.references :mesa, foreign_key: true
+      t.references :comanda, foreign_key: true
       t.references :produto, foreign_key: true
       t.integer :quantidade
       t.datetime :data_hora_entrega
